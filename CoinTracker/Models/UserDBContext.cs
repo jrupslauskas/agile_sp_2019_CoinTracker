904194dab9ca356b53b7ad81678fd8981b4005c5
Joe Rup <jrupslauskas@gmail.com>
2019-03-02 11:09:29 -0600

    Entity added

---
+++ CoinTracker/Models/UserDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+namespace CoinTracker.Models
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext()
+        {
+        }
+    }
+}
